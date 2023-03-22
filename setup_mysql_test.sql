7c1765b6b03480cad8feecaa891d76b1c0edc72d
LocalLso <maphalethulocupadadeepmakhele5@gmail.com>
2023-03-22 22:40:27 +0000

    MySQL setup test.

--- setup_mysql_test.sql
+++ setup_mysql_test.sql
@@ -5,15 +5,8 @@
 --   Grants SELECT privilege for hbnb_test on performance_schema.
 
 CREATE DATABASE IF NOT EXISTS hbnb_test_db;
-CREATE USER
-    IF NOT EXISTS 'hbnb_test'@'localhost'
-    IDENTIFIED BY 'hbnb_test_pwd';
-GRANT ALL PRIVILEGES
-   ON `hbnb_test_db`.*
-   TO 'hbnb_test'@'localhost'
-   IDENTIFIED BY 'hbnb_test_pwd';
-GRANT SELECT
-   ON `performance_schema`.*
-   TO 'hbnb_test'@'localhost'
-   IDENTIFIED BY 'hbnb_test_pwd';
+CREATE USER IF NOT EXISTS 'hbnb_test'@'localhost' IDENTIFIED BY 'hbnb_test_pwd';
+GRANT ALL PRIVILEGES ON `hbnb_test_db`.* TO 'hbnb_test'@'localhost' IDENTIFIED BY 'hbnb_test_pwd';
+FLUSH PRIVILEGES;
+GRANT SELECT ON `performance_schema`.* TO 'hbnb_test'@'localhost' IDENTIFIED BY 'hbnb_test_pwd';
 FLUSH PRIVILEGES;
